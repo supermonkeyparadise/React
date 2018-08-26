2f03436ca0e528485fc255e5473d2ec8effb893a
Unknown <steven.chou@uniqestate.com>
2018-08-26 14:57:16 +0800

    Extracting TableHeader

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,45 +1,29 @@
 import React, { Component } from 'react';
 
 import Like from './common/like';
+import TableHeader from './common/tableHeader';
 
 class MoviesTable extends Component {
-  raiseSort = path => {
-    const sortColumn = { ...this.props.sortColumn };
-
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === 'asc' ? 'desc' : 'asc';
-    else {
-      sortColumn.path = path;
-      sortColumn.order = 'asc';
-    }
-
-    this.props.onSort(sortColumn);
-  };
+  columns = [
+    { key: '#', label: '#' },
+    { path: 'title', label: 'Title' },
+    { path: 'genre.name', label: 'Genre' },
+    { path: 'numberInStock', label: 'Stock' },
+    { path: 'dailyRentalRate', label: 'Rate' },
+    { key: 'like' },
+    { key: 'delete' }
+  ];
 
   render() {
-    const { movies, onDelete, onLike } = this.props;
+    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
 
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th onClick={() => this.raiseSort('title')} scope="col">
-              Title
-            </th>
-            <th onClick={() => this.raiseSort('genre.name')} scope="col">
-              Genre
-            </th>
-            <th onClick={() => this.raiseSort('numberInStock')} scope="col">
-              Stock
-            </th>
-            <th onClick={() => this.raiseSort('dailyRentalRate')} scope="col">
-              Rate
-            </th>
-            <th scope="col" />
-            <th scope="col" />
-          </tr>
-        </thead>
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
         <tbody>
           {movies.map((movie, index) => {
             return (
