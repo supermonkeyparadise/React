4a3826751d17f4a2ad7e11723660c6cd881b149f
Unknown <steven.chou@uniqestate.com>
2018-08-26 20:23:17 +0800

    Passing Props

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -18,7 +18,10 @@ class App extends Component {
         <NavBar />
         <div className="content">
           <Switch>
-            <Route path="/products" component={Products} />
+            <Route
+              path="/products"
+              render={props => <Products sortBy="newst" {...props} />}
+            />
             <Route path="/posts" component={Posts} />
             <Route path="/admin" component={Dashboard} />
             <Route path="/" component={Home} />
