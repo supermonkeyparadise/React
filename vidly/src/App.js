ee7318b43ddd53094647e49a4208fc0b79634972
Unknown <steven.chou@uniqestate.com>
2018-08-29 18:44:06 +0800

    Controlled Elements

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -3,6 +3,7 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Movies from './components/movies';
 import MovieForm from './components/movieForm';
+import LoginForm from './components/common/loginForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
@@ -17,6 +18,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
---
+++ vidly/src/components/common/loginForm.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // Call the server
+    console.log('>>> Submitted');
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              id="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default LoginForm;
