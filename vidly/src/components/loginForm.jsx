71a4db16b7e2ce04426601a9222b069e3acfcb01
Unknown <steven.chou@uniqestate.com>
2018-08-31 19:43:14 +0800

    Validating a Form Using Joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
 
 import Input from './common/input';
 
@@ -8,19 +9,26 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label('Username'),
+    password: Joi.string()
+      .required()
+      .label('Password')
+  };
+
   validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    console.log('>>> joi result:', error);
+
+    if (!error) return null;
+
     const errors = {};
-    const { account } = this.state;
+    for (let item of error.details) errors[item.path[0]] = item.message;
 
-    if (account.username.trim() === '') {
-      errors.username = 'Username is required.';
-    }
-
-    if (account.password.trim() === '') {
-      errors.password = 'Password is required.';
-    }
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    return errors;
   };
 
   validateProperty = ({ name, value }) => {
