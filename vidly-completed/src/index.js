c71a40c88ba504c02200c49bbc99ff05f507fc5c
Unknown <steven.chou@uniqestate.com>
2018-09-09 17:41:49 +0800

    Environment Variables

--- vidly-completed/src/index.js
+++ vidly-completed/src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+console.log('>>> process.env', process.env);
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 registerServiceWorker();
